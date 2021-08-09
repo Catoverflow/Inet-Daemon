95c01fcdcc8dca45bb2c90a0d8b9f0ba285c70c2
Catoverflow <>
2021-08-09 19:40:18 +0800

    minor update

---
+++ tools/confupdate.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# daemon service, update config from github repo
+
+import os
+import json
+import requests
+import datetime
+
+git_repo = 'user/repo'
+interval = 30  # commits in interval seconds will be ignored
+
+
+def get_uid():
+    try:
+        uid = os.popen('id').read()
+        uid = int(uid.split('=')[1].split('(')[0])
+    except:
+        uid = None
+    return uid
+
+def query_latest_commit():
+    url = f'https://api.github.com/repos/{git_repo}/commits'
+    r = requests.get(url=url, headers={
+                     'Accept': 'application/vnd.github.v3+json'}, params={'per_page': 1})
+    r.encoding = 'utf-8'
+    if r.status_code != 200:
+        return None
+    content = json.loads(r.content)
+    try:
+        commit_time = content[0]['commit']['committer']['date']
+    except:
+        return None
+    # both is set to UTC +0
+    time_now = datetime.datetime.utcnow()
+    commit_time = datetime.datetime.strptime(commit_time, '%Y-%m-%dT%H:%M:%SZ')
+    delta = (time_now-commit_time).total_seconds()
+    if delta > interval:
+        return True
+    else:
+        return False
+
+def update_config():
+    os.chdir('/etc/mesh')
+    return_code = os.popen('git pull -q')
+    if return_code != 0:
+        return False
+    else:
+        return True
+
+if __name__ == '__main__':
+    pass
--- tools/wgconfgen.py
+++ tools/wgconfgen.py
@@ -1,3 +1,49 @@
 # generate new wg config after pulling from github repo
 import yaml
-pass
+from requests import get
+
+ip_lookup = ['ipv4.icanhazip.com', 'api.ipify.org',
+             'v4.ident.me', 'ipv4bot.whatismyipaddress.com']
+
+
+def get_wan_ip():
+    ip_address = None
+    for service in ip_lookup:
+        try:
+            ip_address = get(f'http://{service}',
+                             timeout=5).text.split('\n')[0]
+            break
+        except:
+            pass
+    return ip_address
+
+
+def load_config(path='./config.yml'):
+    try:
+        conf = yaml.load(open(path, 'r'), Loader=yaml.SafeLoader)
+        return conf
+    except:
+        return None
+
+
+def wgconf_gen(conf, selfip, selfprivkey):
+    wgconf = {}
+    wgconf['Peer'] = []
+    try:
+        for peer in conf:
+            if peer['WANaddr'] == selfip:
+                wgconf['Interface'] = {'Address': peer['LANaddr'], 'SaveConfig': 'true',
+                                    'PrivateKey': selfprivkey, 'ListenPort': peer['WGport']}
+            else:
+                wgconf['peer'].append({'PublicKey': peer['WGpubkey'], 'AllowedIPs': peer['LANaddr'],
+                                    'Endpoint': '{}:{}'.format(peer['WANaddr'], peer['WGport'])})
+        return wgconf
+    except:
+        return None
+
+
+def get_privatekey():
+    pass
+
+if __name__ == '__main__':
+    pass
